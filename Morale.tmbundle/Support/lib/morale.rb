c09b2968ced0460f670b6975cd82807fa36899cb
Jamie Wright <jamie@brilliantfantastic.com>
2011-12-15 00:23:08 -0500

    Added credential parsing to the file

--- Morale.tmbundle/Support/lib/morale.rb
+++ Morale.tmbundle/Support/lib/morale.rb
@@ -5,6 +5,13 @@ require 'morale/client'
 
 comment = ARGV[0]
 
+#TODO: Store the subdomain, api key, and project # (use same storage as CLI)
+#TODO: Use the morale/storage stuff && morale/credentials_store
+#TODO: Check that the credentials are there
+# Get the subdomain from the .morale directory
+credentials = File.read "#{ENV['HOME']}/.morale/credentials"
+creds = credentials.split("\n") if credentials
+
 # Remove the comment characters, any leading and trailing spaces, carriage returns, and line feeds
 cleansed = ''
 comment.each_line do |line|
@@ -12,12 +19,8 @@ comment.each_line do |line|
 end
 cleansed.strip!
 
-#TODO: Store the subdomain, api key, and project # (use same storage as CLI)
-client = Morale::Client.new('bf', '')
-# client.projects.each do |project|
-#   puts "Project: #{project['project']['name']}"
-# end
-t = client.ticket(100, cleansed)
+client = Morale::Client.new(creds[0], creds[1])
+t = client.ticket(creds[2].to_i, cleansed)
 ticket = t['task'] unless t['task'].nil?
 ticket = t['bug'] unless t['bug'].nil?
 ticket = t if ticket.nil?
