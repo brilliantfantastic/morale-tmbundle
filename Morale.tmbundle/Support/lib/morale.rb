57b41f0009a20695d3e3f8cc30dfb0f7454ba608
Jamie Wright <jamie@brilliantfantastic.com>
2011-12-17 00:26:55 -0500

    Added the ability to update a ticket

--- Morale.tmbundle/Support/lib/morale.rb
+++ Morale.tmbundle/Support/lib/morale.rb
@@ -15,9 +15,21 @@ creds = credentials.split("\n") if credentials
 # Remove the comment characters, any leading and trailing spaces, carriage returns, and line feeds
 cleansed = ''
 comment.each_line do |line|
-  cleansed << line.gsub(/^(\# )|(\#)/, ' ').rstrip.chomp
+  cleansed << line.gsub(/^(\# )|^(\#)/, ' ').rstrip.chomp
 end
 cleansed.strip!
+
+# See if this is an update
+matches = /( \(#\d+\))/.match(cleansed)
+unless matches.nil?
+  suffix = matches[0]
+  matches = /\d+/.match(suffix)
+  unless matches.nil?
+    id = matches[0]
+    cleansed = "u ##{id}: #{cleansed.gsub(suffix, '')}"
+    comment = comment.gsub(suffix, '')
+  end
+end
 
 client = Morale::Client.new(creds[0], creds[1])
 t = client.ticket(creds[2].to_i, cleansed)
@@ -25,4 +37,4 @@ ticket = t['task'] unless t['task'].nil?
 ticket = t['bug'] unless t['bug'].nil?
 ticket = t if ticket.nil?
 
-puts "Hello! You are supposed to process the comment '#{cleansed}' with ticket ##{ticket['identifier']}"
+puts "#{comment} (##{ticket['identifier']})"
